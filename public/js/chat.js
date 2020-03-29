523b1bcf8cca2a9581696869fd3146a78d8f1ac5
aashish <aashish@ubuntu>
2020-03-29 16:35:58 +0545

    Setting up environment for Mustache script

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,9 +4,15 @@ const socket = io();
 const $messageForm = document.querySelector('#message-form');
 const $messageFormInput = $messageForm.querySelector('input');
 const $messageFormButton = $messageForm.querySelector('button');
+const $messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
 
 socket.on('message', (message) => {
     console.log(message);
+    const html = Mustache.render(messageTemplate, { message });
+    $messages.insertAdjacentHTML('beforeend', html);
 });
 
 $messageForm.addEventListener('submit', (e) => {
@@ -21,7 +27,7 @@ $messageForm.addEventListener('submit', (e) => {
         // Enable button again
         $messageFormButton.removeAttribute('disabled');
 
-        $messageFormInput.valueup = '';
+        $messageFormInput.value = '';
         $messageFormInput.focus();
         if(error){
             return console.log(error);
