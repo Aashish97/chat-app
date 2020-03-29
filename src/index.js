8ccf4280a18b443d2a95c8f1e18389e8006249f9
aashish <aashish@ubuntu>
2020-03-29 15:48:59 +0545

    Profanity message setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const publicDirectoryPath = path.join(__dirname, './../public');
 const port = process.env.PORT || 3000;
@@ -18,8 +19,16 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('message', 'A new user has joined!');
 
-    socket.on('sendMessage', (message) => {
+    // callback recieves the acknowledgement whether the message is delivered or not
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter();
+
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed')
+        }
+
         io.emit('message', message);
+        callback();
     })
 
     socket.on('disconnect', () => {
