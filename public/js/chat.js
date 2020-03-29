5da5b10a11d0c77c5db0640570773d73cd556469
aashish <aashish@ubuntu>
2020-03-29 15:49:45 +0545

    Profanity message setup and messed up with form data

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,32 @@
 const socket = io();
 
+// Elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+
 socket.on('message', (message) => {
     console.log(message);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    //Disable button when msg is sent
+    $messageFormButton.setAttribute('disabled','disabled');
+
     const message = e.target.elements.message.value;
-    socket.emit('sendMessage', message);
+    socket.emit('sendMessage', message, (error) => {
+
+        // Enable button again
+        $messageFormButton.removeAttribute('disabled');
+
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
+        if(error){
+            return console.log(error);
+        }
+
+        console.log('Message Delivered!')
+    });
 })
