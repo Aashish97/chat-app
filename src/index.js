1aef953b6516a869687a6b2dafad43f560712598
aashish <aashish@ubuntu>
2020-03-29 14:10:35 +0545

    Added broadcast messages

--- src/index.js
+++ src/index.js
@@ -12,14 +12,18 @@ const io = socketio(server );
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 io.on('connection', (socket) => {
     console.log('New WebSocket connection');
     socket.emit('message', 'Welcome!!!');
 
+    socket.broadcast.emit('message', 'A new user has joined!');
+
     socket.on('sendMessage', (message) => {
         io.emit('message', message);
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
     })
 })
 
